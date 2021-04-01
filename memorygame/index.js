b2901e608abb56026bdcfad41a2b34efc0a5d021
Małgorzata Pańska <47063770+mpanska@users.noreply.github.com>
2021-04-01 13:50:21 +0200

    basic functions implemented

---
+++ memorygame/index.js
@@ -0,0 +1,122 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    // creating the board
+    const board = document.querySelector('#board')
+
+    var guessedCards = [];
+    var guessedCardsIDs = [];
+    var winCards = [];
+    const result = document.querySelector('#result');
+
+    const cards = [
+        {
+            name: 'alien',
+            img: 'images/alien.jpg'
+        },
+        {
+            name: 'banana',
+            img: 'images/banana.jpg'
+        },
+        {
+            name: 'book',
+            img: 'images/book.jpg'
+        },
+        {
+            name: 'cat',
+            img: 'images/cat.jpg'
+        },
+        {
+            name: 'donut',
+            img: 'images/donut.jpg'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.jpg'
+        },
+        {
+            name: 'alien',
+            img: 'images/alien.jpg'
+        },
+        {
+            name: 'banana',
+            img: 'images/banana.jpg'
+        },
+        {
+            name: 'book',
+            img: 'images/book.jpg'
+        },
+        {
+            name: 'cat',
+            img: 'images/cat.jpg'
+        },
+        {
+            name: 'donut',
+            img: 'images/donut.jpg'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.jpg'
+        }
+    ]
+    // randomizing our cards
+    cards.sort(()=> 0.5 - Math.random());
+
+
+    // initialize game board
+    function initBoard(){
+        for(let i = 0; i < cards.length; i++){
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/back.jpg');
+            // set an attribute for card element on the board
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flip);
+            board.appendChild(card);
+        }
+    }
+
+    // check matches
+    function checkMatch(){
+        const allCards = document.querySelectorAll('img');
+        const firstCardID = guessedCardsIDs[0];
+        const secondCardID = guessedCardsIDs[1];
+
+        if(firstCardID == secondCardID){
+            allCards[firstCardID].setAttribute('src', 'images/back.jpg');
+            allCards[secondCardID].setAttribute('src', 'images/back.jpg');
+            alert('it is the same card');
+        }
+        else if(guessedCards[0] === guessedCards[1]){
+            alert('Match!');
+            allCards[firstCardID].setAttribute('src', '');
+            allCards[secondCardID ].setAttribute('src', '');
+            allCards[firstCardID].removeEventListener('click', flip);
+            allCards[secondCardID].removeEventListener('click', flip);
+            winCards.push(guessedCards);
+        }
+        else{
+            allCards[firstCardID].setAttribute('src', 'images/back.jpg');
+            allCards[secondCardID].setAttribute('src', 'images/back.jpg');
+            alert('no match');
+        }
+
+        guessedCards = [];
+        guessedCardsIDs = [];
+        result.textContent = winCards.length;
+        if(winCards.length === cards.length/2){
+            result.textContent = 'You won!!!';
+        }
+    }
+
+    //flip cards
+    function flip(){
+        let cardID = this.getAttribute('data-id');
+        guessedCards.push(cards[cardID].name);
+        guessedCardsIDs.push(cardID);
+        this.setAttribute('src', cards[cardID].img)
+        if(guessedCards.length === 2){
+            setTimeout(checkMatch, 500)
+        }
+    }
+
+    initBoard();
+})
